dcde512f777548c721662412bb2538fdbd1383a1
Jonas Olson <jonas.olson@gmail.com>
2025-11-04 11:31:06 +0100

    Added Vite and Bootstrap

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
+// filepath: /home/jonas/code/js1/Inlämningsuppgift1/code/fed25-js1-uppgift-1-KaptenKatthatt/main.js
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
+const students = [
+  {
+    id: 1,
+    name: "Kalle Kallesson",
+    image: URL("img/jonas_olson.jpg"),
+  },
+];
+
 //Show startscreen
 
 // Ask how many persons you wish to guess on, 5, 10 or ALL?
@@ -8,6 +20,8 @@
 //If ALL, pick array.length number of people and randomize
 
 // Load game screen with 1 photo and four choice buttons, and a disabled next question button.
+
+//make an array with the correct name and three wrong ones. Randomize the array before showing it.
 
 // User clicks on an option
 //  Correct? Change class to correct which colors the button green(bs-success)
