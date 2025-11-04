8474e41296fcdb64dfd0c19dc3e34f9f8fe82eba
Jonas Olson <jonas.olson@gmail.com>
2025-11-04 10:46:05 +0100

    Flow chart and pseudo code added

---
+++ script.js
@@ -0,0 +1,24 @@
+//Show startscreen
+
+// Ask how many persons you wish to guess on, 5, 10 or ALL?
+
+//An event listener on the ul containing the buttons listens for the clicked button and sets answer to that.
+
+//If 5 or 10, pick 5 or 10 random objects from array.
+//If ALL, pick array.length number of people and randomize
+
+// Load game screen with 1 photo and four choice buttons, and a disabled next question button.
+
+// User clicks on an option
+//  Correct? Change class to correct which colors the button green(bs-success)
+//Wrong? Change class to wrong that makes the button red(bs-danger).
+
+//Enable Next question button -> Generates next question
+
+//Update score
+
+//Game finished?
+// No, goto next question
+// Yes, show finishscreen with score and high score.
+//Check If highschore, if yes update highscore
+//Show Play again button -> Goes to startscreen
