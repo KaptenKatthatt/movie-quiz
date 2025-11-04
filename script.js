a89882906bbdb0fc1848384c3a5744c4c0028f55
Jonas Olson <jonas.olson@gmail.com>
2025-11-04 14:41:33 +0100

    Added eventlistener that slices array depending on nbr of selected photos to guess

--- script.js
+++ script.js
@@ -1,116 +1,140 @@
 const selectFiveBtnEl = document.querySelector(".selectFiveBtn");
 const selectTenBtnEl = document.querySelector(".selectTenBtn");
 const selectAllBtnEl = document.querySelector(".selectAllBtn");
-const btnContainerEl = document.querySelector(".btnContainer");
+const startBtnContainerEl = document.querySelector(".startBtnContainer");
+const questionScreenContainerEl = document.querySelector(
+  ".questionScreenContainer"
+);
 
 const students = [
   {
     id: 1,
     name: "Kalle Kallesson",
-    image: URL("img/jonas_olson.jpg"),
+    image: "img/jonas_olson.jpg",
   },
   {
     id: 2,
     name: "Anna Andersson",
-    image: URL("img/anna_andersson.jpg"),
+    image: "img/anna_andersson.jpg",
   },
   {
     id: 3,
     name: "Erik Eriksson",
-    image: URL("img/erik_eriksson.jpg"),
+    image: "img/erik_eriksson.jpg",
   },
   {
     id: 4,
     name: "Maria Nilsson",
-    image: URL("img/maria_nilsson.jpg"),
+    image: "img/maria_nilsson.jpg",
   },
   {
     id: 5,
     name: "Johan Johansson",
-    image: URL("img/johan_johansson.jpg"),
+    image: "img/johan_johansson.jpg",
   },
   {
     id: 6,
     name: "Lisa Larsson",
-    image: URL("img/lisa_larsson.jpg"),
+    image: "img/lisa_larsson.jpg",
   },
   {
     id: 7,
     name: "Peter Petersson",
-    image: URL("img/peter_petersson.jpg"),
+    image: "img/peter_petersson.jpg",
   },
   {
     id: 8,
     name: "Sara Svensson",
-    image: URL("img/sara_svensson.jpg"),
+    image: "img/sara_svensson.jpg",
   },
   {
     id: 9,
     name: "Mikael Karlsson",
-    image: URL("img/mikael_karlsson.jpg"),
+    image: "img/mikael_karlsson.jpg",
   },
   {
     id: 10,
     name: "Emma Gustafsson",
-    image: URL("img/emma_gustafsson.jpg"),
+    image: "img/emma_gustafsson.jpg",
   },
   {
     id: 11,
     name: "David Davidsson",
-    image: URL("img/david_davidsson.jpg"),
+    image: "img/david_davidsson.jpg",
   },
   {
     id: 12,
     name: "Jenny Persson",
-    image: URL("img/jenny_persson.jpg"),
+    image: "img/jenny_persson.jpg",
   },
   {
     id: 13,
     name: "Magnus Olsson",
-    image: URL("img/magnus_olsson.jpg"),
+    image: "img/magnus_olsson.jpg",
   },
   {
     id: 14,
     name: "Camilla Jonsson",
-    image: URL("img/camilla_jonsson.jpg"),
+    image: "img/camilla_jonsson.jpg",
   },
   {
     id: 15,
     name: "Fredrik Lindberg",
-    image: URL("img/fredrik_lindberg.jpg"),
+    image: "img/fredrik_lindberg.jpg",
   },
   {
     id: 16,
     name: "Helena Holm",
-    image: URL("img/helena_holm.jpg"),
+    image: "img/helena_holm.jpg",
   },
 ];
 
-btnContainerEl.addEventListener("click", (e) => {
-  //An event listener on the ul containing the buttons listens for the clicked button and sets answer to that.
-});
+let nbrOfSelected = 0;
 
-console.log(students);
+console.log("Students before shuffle", students);
 
 // Fishes-Yates algoritm for array shuffling to the rescue! 🤩
-const shuffleArray = (array) => {
-  for (let i = array.length - 1; i > 0; i--) {
+const cloneAndShuffleArray = (array) => {
+  const shuffledArrayClone = [...array];
+  for (let i = shuffledArrayClone.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
-    const temp = array[i];
-    array[i] = array[j];
-    array[j] = temp;
+    const temp = shuffledArrayClone[i];
+    shuffledArrayClone[i] = shuffledArrayClone[j];
+    shuffledArrayClone[j] = temp;
   }
+  return shuffledArrayClone;
 };
 
-console.log(shuffleArray(students));
+// Randomize array
+//Clone and shuffle students array
+const shuffledStudents = cloneAndShuffleArray(students);
+console.log("Students after shuffle", shuffledStudents);
 
 //Show startscreen
 
 // Ask how many persons you wish to guess on, 5, 10 or ALL?
-
-// Randomize array
-
+//An event listener on the container containing the buttons listens for the clicked button and sets answer to that.
 // Filter out 5,10 or all
+
+startBtnContainerEl.addEventListener("click", (e) => {
+  if (e.target.textContent.includes("5")) {
+    nbrOfSelected = 5;
+    console.log("5 selected", nbrOfSelected);
+    console.log(shuffledStudents.slice(0, nbrOfSelected));
+  } else if (e.target.textContent.includes("10")) {
+    nbrOfSelected = 10;
+    console.log("10 selected", nbrOfSelected);
+    console.log(shuffledStudents.slice(0, nbrOfSelected));
+  } else if (e.target.textContent.includes("Yes")) {
+    nbrOfSelected = students.length;
+    console.log("All selected", nbrOfSelected);
+    console.log(shuffledStudents.slice(0, nbrOfSelected));
+  }
+  startBtnContainerEl.classList.add("d-none");
+  questionScreenContainerEl.classList.remove("d-none");
+});
+
+const gameArray = [];
 
 // Map the results to buttons
 
