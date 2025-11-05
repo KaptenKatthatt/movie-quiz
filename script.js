3cc8164b801f20387e8087cbf2c8316d83a7e359
Jonas Olson <jonas.olson@gmail.com>
2025-11-05 11:16:21 +0100

    Check if worthy of high score function created.

--- script.js
+++ script.js
@@ -159,20 +159,48 @@ function renderEndScreen() {
   endScoreEl.innerText = `You final score is ${finalScore}/${nbrOfSelectedStudents}`;
 
   // Check if finalScore is higher than lowest highscore
-  rUWorthy = highscoreList.find((score) => {
-    finalScore >= score;
-  });
-  console.log("rUWorthy", rUWorthy);
+  // Remove lowest score and replace with finalScore
+  // Find lowest highscore
+  let highscoreList = [5, 2, 3];
+  let previousTry = finalScore;
+  let rUWorthy = false;
+  // endScoreEl.innerText = `You final score is ${finalScore}/${nbrOfSelectedStudents}`;
 
-  // Add score to top3 if higher than lowest score
+  // Check if finalScore is higher than lowest highscore
+  // Remove lowest score and replace with finalScore
+  // Find lowest highscore
+  let lowestHighScore = Math.min(...highscoreList);
+  console.log("Lowest highscore", lowestHighScore);
+  let highestHighScore = Math.max(...highscoreList);
+  console.log("Highest highscore", highestHighScore);
+
+  // Check if high score worthy, removes lowest score
+  if (finalScore > lowestHighScore) {
+    highscoreList.splice(
+      highscoreList.findIndex((score) => lowestHighScore < finalScore),
+      1
+    );
+    // Add score to high score list and sort list
+    highscoreList.push(finalScore);
+    highscoreList.sort((a, b) => b - a);
+    // r U worthy of high score?
+    rUWorthy = true;
+  }
+  // Check if finalScore is the highest score
+  if (finalScore > highestHighScore) {
+    console.log("Winner winner chicken dinner! Highest score!", finalScore);
+  }
+
+  console.log("rUWorthy", rUWorthy);
+  console.log("Highscore after", highscoreList);
 
   // Check if high score worthy
-  if (highscoreList.length === 3 && rUWorthy) {
-    highscoreList.push(finalScore).sort();
+  if (finalScore > highestHighScore) {
+    console.log("You got the highest score!");
   } else if (highscoreList.length < 3) {
-    highscoreList.push(finalScore).sort();
+    console.log("You got a high score! List was not full");
   } else {
-    alert("No highscore, do gooder next time plz.");
+    console.log("No highscore, do gooder next time plz.");
   }
 
   // Display correct and wrong answers with name and photo with cards
